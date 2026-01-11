a3709f373724260d0cb690a57e7d3fe89a527378
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-01-11 23:52:57 +0500

    feat : display the word details including definition, synonyms and source Link

--- src/Component/WordSearch.jsx
+++ src/Component/WordSearch.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const WordSearch = () => {
   const [search, setSearch] = useState("");
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   const handleOnchangeEvent = (e) => {
     console.log(e.target.value);
@@ -36,7 +36,17 @@ const WordSearch = () => {
           <button onClick={fetchData}>Search</button>
         </div>
         <div className="datas">
-          
+          {data ? (
+            <div className="datas">
+              <h2>Word : {data.word}</h2>
+              <p>Part Of Speech : {data.meanings[0].partOfSpeech}</p>
+              <p>definition : {data.meanings[0].definitions[0].definition}</p>
+              <p>synonym : {data.meanings[0].synonyms[0]}</p>
+              <button onClick={() => {window.open(data.sourceUrls[0], "_blank");}}>Read More</button>
+            </div>
+          ) : (
+            "Data not found"
+          )}
         </div>
       </div>
     </div>
