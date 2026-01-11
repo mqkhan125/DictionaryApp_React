1b15b25ab6d63b3f90cc70990cd76269503d4c93
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-01-11 22:52:14 +0500

    feat : add search state and fetch dictionary API data

--- src/Component/WordSearch.jsx
+++ src/Component/WordSearch.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const WordSearch = () => {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleOnchangeEvent = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
+
+  const fetchData = async () => {
+    try {
+      let get = await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
+      );
+      let jsonData = await get.json();
+      setData(jsonData[0])
+      console.log(jsonData[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className='app'>
-        <h1>Dictionary App</h1>
-        <div className='container'>
-            <div className='searchBar'>
-                <input type="text" placeholder="Search Word's" />
-                <button>Search</button>
-            </div>
-            <div className="datas">
-
-            </div>
+    <div className="app">
+      <h1>Dictionary App</h1>
+      <div className="container">
+        <div className="searchBar">
+          <input
+            type="text"
+            placeholder="Search Word's"
+            value={search}
+            onChange={handleOnchangeEvent}
+          />
+          <button onClick={fetchData}>Search</button>
         </div>
-
+        <div className="datas">
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default WordSearch;
